74e5b2b8b8bb4e551843af0d6b70af2d31e8e43d
Douglas <douglas.v849@gmail.com>
2026-09-15 18:31:46 -0300

    getting the project off the ground

---
+++ src/main/java/br/com/d3valves/deliverytracker/DeliveryTrackerApplication.java
@@ -0,0 +1,13 @@
+package br.com.d3valves.deliverytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryTrackerApplication.class, args);
+	}
+
+}
